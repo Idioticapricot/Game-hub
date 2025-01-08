4be9d06d9e6dcf98a8f51660b4b86712f724af2e
Idioticapricot <akarthikeyasmail@gmail.com>
2025-01-08 07:01:05 +0530

    Created loading skeletons for genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,17 @@
 import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getcropped from "../Services/image.url";
-
+import GenreListcardSkeleton from "./GenreListSkeleton";
+import CardContainer from "./CardContainer";
+const skeletons = [1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 const GenreList = () => {
-  const { data } = useGenres()
-  return <List> 
+  const { data, isLoading, error } = useGenres()
+  
+    return <List> 
+       {error && <Text>{error}</Text>}
+      {isLoading &&
+          skeletons.map((skeleton) =><CardContainer> <GenreListcardSkeleton key={skeleton} /></CardContainer>)}
+
     {data.map(genre => <ListItem key={genre.id} paddingY={'5px'}><HStack>
       <Image boxSize={'32px'} borderRadius={'8px'} src={getcropped(genre.image_background)}>
          
---
+++ src/components/GenreListSkeleton.tsx
@@ -0,0 +1,15 @@
+import { SkeletonCircle } from '@chakra-ui/react'
+
+
+const GenreListcardSkeleton = () => {
+  return (
+    
+        <SkeletonCircle  size="12"/>
+        
+
+        
+    
+  )
+}
+
+export default GenreListcardSkeleton
