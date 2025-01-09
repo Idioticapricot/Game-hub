00d5d6b2a74ad2fae279721f92237317485f8e53
Idioticapricot <akarthikeyasmail@gmail.com>
2025-01-09 22:50:03 +0530

    plat form selector menu

---
+++ src/components/PlatformselectorSkeleton.tsx
@@ -0,0 +1,16 @@
+import { SkeletonCircle } from '@chakra-ui/react'
+import Platformselector from './Platformselector'
+
+
+const Platformselectorskeleton = () => {
+  return (
+    
+        <SkeletonCircle  size="12"/>
+        
+
+        
+    
+  )
+}
+
+export default Platformselectorskeleton
